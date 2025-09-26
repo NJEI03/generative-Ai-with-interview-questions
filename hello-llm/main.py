669ba0238226157e4f8b0645c874a5276fbd75d7
NJEI03 <njeipierrick@gmail.com>
2025-09-26 13:46:18 +0100

    Added model params for controlling output

--- hello-llm/main.py
+++ hello-llm/main.py
@@ -17,7 +17,9 @@ class Request(BaseModel):
 @app.post('/hello-llm')
 def generate_text(request: Request):
     #Calling the model
-    result = generator(request.prompt, max_length=50)
+    result = generator(request.prompt, max_length=100,
+    num_return_sequences=1, 
+    temperature=0.7, repetition_penalty=1.5, do_sample=True)
     print(result)
     # Return the generated text
     return{
