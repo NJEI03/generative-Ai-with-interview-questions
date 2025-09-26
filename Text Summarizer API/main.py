b766c73cdfcac634317cba147515dc2f3a53bd38
NJEI03 <njeipierrick@gmail.com>
2025-09-26 14:01:48 +0100

    Loaded the summarization pipeline

--- Text Summarizer API/main.py
+++ Text Summarizer API/main.py
@@ -3,3 +3,6 @@ from pydantic import BaseModel
 from transformers import pipeline
 
 app = FastAPI()
+
+#Loading the summarization pipeline
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
