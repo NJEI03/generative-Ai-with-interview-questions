85070369690f81486e9162ba497e3a981df893c1
NJEI03 <njeipierrick@gmail.com>
2025-09-26 13:07:27 +0100

    Created the fast api server and loaing the model from HF

--- hello-llm/main.py
+++ hello-llm/main.py
@@ -2,3 +2,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline  #Easiest way to use
 
+#Creating the app
+app = FastAPI()
+
+#Loading the model once the app starts
+generator = pipeline('text-generation', model='distilgpt2')
