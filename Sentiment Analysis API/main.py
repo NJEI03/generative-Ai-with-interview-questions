e2a013dadaa5d0990024bd38793c2318082786fe
Agentic-JJ-Web3 <njei.private@gmail.com>
2025-09-26 14:38:02 +0100

    Loaded the model

--- Sentiment Analysis API/main.py
+++ Sentiment Analysis API/main.py
@@ -7,5 +7,15 @@ app = FastAPI()
 
 classifier = pipeline("sentiment-analysis", model ="distilbert-base-uncased-finetuned-sst-2-english")
 
-class sentimentRequest(BaseModel):
+class SentimentRequest(BaseModel):
     text: str
+
+@app.post("/sentiment")
+def analyze_sentiment(request:SentimentRequest ):
+    # Modal reruns a list of results for our unique input
+    result = classifier(request.text)[0]
+
+    return{
+        "sentiment": result['label'],
+        "confidence": round(result['score'], 4) # round to 4 decimal places
+    } 
